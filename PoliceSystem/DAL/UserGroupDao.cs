2cee79d1440f0ed124e1a9b5ca5b5e4faf07f850
Jansen,Sam S.J <sam.jansen@student.fontys.nl>
2016-05-10 11:22:14 +0200

    Added UserGroupDao and styled login 2

---
+++ PoliceSystem/DAL/UserGroupDao.cs
@@ -0,0 +1,46 @@
+﻿using PoliceSystem.Models;
+using PoliceSystem.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliceSystem.DAL
+{
+    interface UserGroupDao
+    {
+        /// <summary>
+        /// Add the given UserGroup as new record to the database.
+        /// </summary>
+        /// <param name="userGroup">to add to the database</param>
+        void Create(UserGroup userGroup);
+
+        /// <summary>
+        /// Find a UserGroup in the database by its Id.
+        /// </summary>
+        /// <param name="id">the Id to check for</param>
+        /// <returns>The found UserGroup</returns>
+        UserGroup FindById(int id);
+
+        /// <summary>
+        /// Find a UserGroup in the database by its Name.
+        /// </summary>
+        /// <param name="name">the Name to check for</param>
+        /// <returns>The found UserGroup</returns>
+        UserGroup FindByName(string name);
+
+        /// <summary>
+        /// Remove a UserGroup from the database.
+        /// </summary>
+        /// <param name="userGroup">The usergroup to remove</param>
+        void Remove(UserGroup userGroup);
+
+        /// <summary>
+        /// Add a User to a UserGroup.
+        /// </summary>
+        /// <param name="userGroup">to add the User to</param>
+        /// <param name="user">to add to the UserGroup</param>
+        void AddToUserGroup(UserGroup userGroup, User user);
+    }
+}
---
+++ PoliceSystem/DAL/UserGroupDaoImpl.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PoliceSystem.Models;
+using PoliceSystem.Models.Domain;
+
+namespace PoliceSystem.DAL
+{
+    public class UserGroupDaoImpl : UserGroupDao
+    {
+
+        public void Create(UserGroup userGroup)
+        {
+            using (PoliceDbContext db = new PoliceDbContext())
+            {
+                if (FindByName(userGroup.Name) == null)
+                {
+                    db.UserGroups.Add(userGroup);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    //laat de gebruiker weten dat de username bezet is
+                }
+
+            }
+        }
+
+        public UserGroup FindById(int id)
+        {
+            using (PoliceDbContext db = new PoliceDbContext())
+            {
+                return db.UserGroups.Single(ug => ug.Id == id);
+            }
+        }
+
+        public UserGroup FindByName(string name)
+        {
+            using (PoliceDbContext db = new PoliceDbContext())
+            {
+                return db.UserGroups.Single(ug => ug.Name == name);
+            }
+        }
+
+        public void Remove(UserGroup userGroup)
+        {
+            using (PoliceDbContext db = new PoliceDbContext())
+            {
+                db.UserGroups.Remove(userGroup);
+            }
+        }
+
+        public void AddToUserGroup(UserGroup userGroup, User user)
+        {
+            using (PoliceDbContext db = new PoliceDbContext())
+            {
+                userGroup.Users.Add(user);
+
+                db.UserGroups.Attach(userGroup);
+                var userGroupEntry = db.Entry(userGroup);
+                userGroupEntry.Property(ug => ug.Users).IsModified = true;
+
+                user.UserGroup = userGroup;
+
+                db.Users.Attach(user);
+                var userEntry = db.Entry(user);
+                userEntry.Property(u => u.UserGroup).IsModified = true;
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
