7f12711b1764897b1a8211402f144c2bcd10bfa1
Jansen,Sam S.J. <sam.jansen@student.fontys.nl>
2016-05-31 15:36:04 +0200

    Added datepickers to map

---
+++ PoliceSystem/Models/ViewModels/MapViewModel.cs
@@ -0,0 +1,31 @@
+﻿using PoliceSystem.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PoliceSystem.Models.ViewModels
+{
+    public class MapViewModel
+    {
+
+        public Car Car { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
+        public DateTime Start { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
+        public DateTime End { get; set; }
+
+        public MapViewModel()
+        {
+            this.Start = DateTime.Now;
+            this.End = DateTime.Now;
+        }
+
+        public MapViewModel(Car car) : base()
+        { 
+            this.Car = car;
+        }
+
+    }
+}
