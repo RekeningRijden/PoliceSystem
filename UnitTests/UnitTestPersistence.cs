e730b7e66b7ea25b1faf3a276fb64a8c0e8fa8d7
Eric <ericderegter@gmail.com>
2016-06-24 13:58:30 +0200

    added unittest

---
+++ UnitTests/UnitTestPersistence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PoliceSystem.Controllers;
+using PoliceSystem.DAL;
+using PoliceSystem.Models.Domain;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTestPersistence
+    {
+        [TestMethod]
+        public void TestMethodAddUser()
+        {
+            UserService userService = new UserService();
+            userService.Create(new User() { Username = "test@test.nl", Password = "test" });
+            User user = userService.FindByUsername("test@test.nl");
+            Assert.IsNotNull(user);
+            Assert.AreEqual(user.Username, "test@test.nl");
+        }
+    }
+}
